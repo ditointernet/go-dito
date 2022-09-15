c8a00db00f495827036e3ba03b76585d4534a6fc
PimentaJoao <jppimenta.pessoal@gmail.com>
2022-09-14 22:41:39 -0300

    refactor: simplify instatiation process, requiring less dependencies

--- pubsub/subscriber.go
+++ pubsub/subscriber.go
@@ -3,37 +3,15 @@ package pubsub
 import (
 	"context"
 
-	"cloud.google.com/go/pubsub"
 	"github.com/ditointernet/go-dito/errors"
 	steps "github.com/ditointernet/go-dito/pubsub/subscriber_steps"
 )
 
-// DefaultReceiveSettings defines default settings for subscription ReceiveSettings.
-type DefaultReceiveSettings struct {
-	maxOutstandingMessages int
-	Synchronous            bool
-}
-
-// DefaultReceiveSettings holds the default values for DefaultReceiveSettings.
-var DefaultReceiveSettingsValues = DefaultReceiveSettings{
-	maxOutstandingMessages: 500,
-	Synchronous:            true,
-}
-
 // PubsubSubscriberPipelineParams encapsulates dependencies for a SubscriberPipelineParams instance.
 type SubscriberPipelineParams struct {
-	// PubsubClient is a Pubsub client, which interacts with Pubsub resources.
-	PubsubClient *pubsub.Client
+	PubsubSubscription steps.PubsubSubscription
 
-	// SubID is the unique identifier of the subscription within its project.
-	SubID string
-
-	// MaxOutstandingMessages is the maximum number of unprocessed messages
-	// (unacknowledged but not yet expired). If MaxOutstandingMessages is 0,
-	// the default value of DefaultReceiveSettingsValues.maxOutstandingMessages will be applied.
-	// If the value is negative, then there will be no limit on the number of
-	// unprocessed messages.
-	MaxOutstandingMessages int
+	errCh chan error
 }
 
 // SubscriberPipeline is a structure that defines a pubsub pipeline data handler.
@@ -58,32 +36,23 @@ type subscriberPipeline struct {
 	steps []PipelineStep
 }
 
-// NewPubsubSubscriberPipeline creates a new instance of pubsubSubscriberPipeline.
+// NewSubscriberPipeline creates a new instance of subscriberPipeline.
 // The pipeline initiates with only one step: subscriberReceiver (which receives raw messages from Pubsub).
 func NewSubscriberPipeline(params SubscriberPipelineParams) (subscriberPipeline, error) {
-	if params.PubsubClient == nil {
-		return subscriberPipeline{}, errors.NewMissingRequiredDependency("PubsubClient")
+	if params.PubsubSubscription == nil {
+		return subscriberPipeline{}, errors.NewMissingRequiredDependency("PubsubSubscription")
 	}
 
-	if params.SubID == "" {
-		return subscriberPipeline{}, errors.NewMissingRequiredDependency("SubID")
+	if params.errCh == nil {
+		params.errCh = make(chan error)
 	}
-
-	maxOutstandingMessages := DefaultReceiveSettingsValues.maxOutstandingMessages
-	if params.MaxOutstandingMessages == 0 {
-		maxOutstandingMessages = params.MaxOutstandingMessages
-	}
-
-	sub := params.PubsubClient.Subscription(params.SubID)
-	sub.ReceiveSettings.MaxOutstandingMessages = maxOutstandingMessages
-	sub.ReceiveSettings.Synchronous = DefaultReceiveSettingsValues.Synchronous
 
 	firstStep := steps.SubscriberReceiver{
-		Subscription: sub,
+		Subscription: params.PubsubSubscription,
 	}
 
 	sp := subscriberPipeline{
-		errCh: make(chan error),
+		errCh: params.errCh,
 		steps: []PipelineStep{
 			firstStep,
 		},
@@ -104,7 +73,7 @@ func MustNewSubscriberPipeline(params SubscriberPipelineParams) subscriberPipeli
 }
 
 // Run kicks off all pipeline steps executions, starting the subscription message receiving
-// process and connecting each registered step in a ordered way.
+// process and connecting each additional registered step in a ordered way.
 func (sp subscriberPipeline) Run(ctx context.Context) chan any {
 	// Spins up the receiver, which retrieves raw pubsub messages.
 	ch := sp.steps[0].Do(ctx, nil, sp.errCh)
@@ -114,7 +83,7 @@ func (sp subscriberPipeline) Run(ctx context.Context) chan any {
 		ch = sp.steps[ii].Do(ctx, ch, sp.errCh)
 	}
 
-	// Fully configured channel, of which messages go through all pipeline steps.
+	// Fully configured channel, with messages that go through all pipeline steps.
 	return ch
 }
 
